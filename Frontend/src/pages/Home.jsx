99dc28bff29aa6a35ab9b3a23175dd6f13b4287a
Ashmit Singh <ashmitsingh1310@gmail.com>
2025-07-13 17:07:19 +0530

    Create Home Page

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+const Home = () => {
+  return (
+    <>
+    <div className="h-screen w-full bg-[url('/images/Uber-Home.png')] bg-cover bg-center flex flex-col justify-between pt-8">
+        <img className="w-16 ml-8" src="/images/Uber-Logo.png" alt="" />
+        <div className="bg-white pb-7 py-4 px-4">
+            <h2 className="text-3xl font-bold">Get Started With Uber</h2>
+            <Link to='/users/login' className=" flex items-center justify-center w-full bg-black text-white py-3 rounded-md mt-4">Continue</Link>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
