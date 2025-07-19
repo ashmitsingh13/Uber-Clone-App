af2b9faf6c00e98cacab6840e758c4cc120c6645
Ashmit Singh <ashmitsingh1310@gmail.com>
2025-07-19 17:23:51 +0530

    Protect Captain Login

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
-app.use('/users', userRoutes);
-app.use('/captains', captainRoutes);
+app.use('/user', userRoutes);
+app.use('/captain', captainRoutes);
 
 module.exports = app;
