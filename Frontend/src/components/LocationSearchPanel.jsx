852be15b1bb19113102abdc83745b457e748a8e4
Ashmit Singh <ashmitsingh1310@gmail.com>
2025-07-21 21:14:46 +0530

    Add New Features

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+        <div className='flex items-center justify-start gap-4 my-4'>
+            <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+            <h4 className='font-medium'>24B, Near Kapoor's cafe ,Sherians Coding School , Bhopal</h4>
+        </div>
+        <div className='flex items-center justify-start gap-4 my-4'>
+            <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+            <h4 className='font-medium'>24B, Near Kapoor's cafe ,Sherians Coding School , Bhopal</h4>
+        </div>
+        <div className='flex items-center justify-start gap-4 my-4'>
+            <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+            <h4 className='font-medium'>24B, Near Kapoor's cafe ,Sherians Coding School , Bhopal</h4>
+        </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,40 +1,59 @@
 import React, { useRef, useState } from "react";
-import {useGSAP} from "@gsap/react"
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import "remixicon/fonts/remixicon.css";
+import LocationSearchPanel from "../components/LocationSearchPanel";
+
 const Home = () => {
   const [pickup, setPickup] = useState("");
   const [destination, setDestination] = useState("");
-  const [panelOpen, setPanelOpen] = useState(false)
-  const panelRef = useRef(null)
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
+
   const submitHandler = (e) => {
     e.preventDefault();
   };
 
-
-  useGSAP(()=>{
-    if(panelOpen){
-        gsap.to(panelRef.current,{
-          height:'70%'
-        })
-      } else{
-      gsap.to(panelRef.current,{
-        height:"0%"
-      })
+  useGSAP(() => {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: "70%",
+        padding: 24,
+      });
+      gsap.to(panelCloseRef.current, {
+        opacity: 1,
+      });
+    } else {
+      gsap.to(panelRef.current, {
+        height: "0%",
+        padding: 0,
+      });
+      gsap.to(panelCloseRef.current, {
+        opacity: 0,
+      });
     }
-  },[panelOpen]
-)
-
+  }, [panelOpen]);
 
   return (
     <>
-      <div className="h-screen w-full bg-[url('/images/UserHomeImg.avif')] bg-cover bg-center flex flex-col justify-between pt-8 relative">
+      <div className="h-screen w-full bg-[url('/images/UserHomeImg.avif')] bg-cover bg-center flex flex-col justify-between pt-8 relative overflow-hidden">
         <img
           className="w-16 ml-8"
           src="/images/Uber-Logo.png"
           alt="Uber Logo"
         />
-        <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
-          <div className="h-[30%] p-6 bg-white relative">
+        <div className="flex flex-col justify-end h-screen absolute top-0 w-full rounded-2xl">
+          <div className="h-[30%] p-6 bg-white relative bottom-0">
+            <h5
+              ref={panelCloseRef}
+              onClick={() => {
+                setPanelOpen(false);
+              }}
+              className="absolute opacity-0 right-6 text-2xl top-6"
+            >
+              <i className="ri-arrow-down-wide-line"></i>
+            </h5>
             <h4 className="text-2xl font-semibold">Find a trip</h4>
 
             <form
@@ -44,10 +63,9 @@ const Home = () => {
             >
               <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-900 rounded-full"></div>
               <input
-              onClick={()=>{
-                setPanelOpen(true)
-              }
-              }
+                onClick={() => {
+                  setPanelOpen(true);
+                }}
                 value={pickup}
                 onChange={(e) => {
                   setPickup(e.target.value);
@@ -57,11 +75,10 @@ const Home = () => {
                 placeholder="Add a pick-up location"
               />
               <input
-              onClick={()=>{
-                setPanelOpen(true)
-              }
-              }
-              value={destination}
+                onClick={() => {
+                  setPanelOpen(true);
+                }}
+                value={destination}
                 onChange={(e) => {
                   setDestination(e.target.value);
                 }}
@@ -71,7 +88,20 @@ const Home = () => {
               />
             </form>
           </div>
-          <div ref={panelRef} className="p-6 bg-white h-0"></div>
+          <div ref={panelRef} className="p-6 bg-white h-0">
+            <LocationSearchPanel />
+          </div>
+        </div>
+        <div className="fixed z-10 bg-white bottom-0 p-5">
+          <div className="flex items-center justify-between">
+            <img className="h-10" src="/images/RedCar.png" alt="" />
+            <div className="">
+              <h4>UberGo <span><i className="ri-user-fill"></i>4</span></h4>
+              <h5>2 mins away</h5>
+              <p>Affordable, compact rides</p>
+            </div>
+            <h2>₹193.20</h2>
+          </div> 
         </div>
       </div>
     </>
